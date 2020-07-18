83ba7cca7a0d7d7e56462bb72cdc852cdeac028d
Meng Li <li.meng@neuralgalaxy.com>
2020-07-19 07:43:36 +0800

    update

--- run_server.sh
+++ run_server.sh
@@ -8,10 +8,6 @@ cd $cwd
 
 echo "Current working directory (for core dumps etc): $cwd"
 
-# Fix "qt.qpa.xcb: could not connect to display"
-# https://github.com/therecipe/qt/blob/master/internal/vagrant/pre.sh#L14-L20
-export QT_QPA_PLATFORM=minimal
-
 if [ "$1" == "-g" ]; then
     gdb ${here}/build/obs_headless_server
 else
